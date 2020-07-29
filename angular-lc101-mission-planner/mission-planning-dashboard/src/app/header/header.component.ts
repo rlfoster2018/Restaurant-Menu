d9631657b499d6e26e13b5d77f81fb8da3b7ff4d
Oracle <rlfoster2018@gmail.com>
2020-07-29 16:05:32 -0500

    Final commitment

--- angular-lc101-mission-planner/mission-planning-dashboard/src/app/header/header.component.ts
+++ angular-lc101-mission-planner/mission-planning-dashboard/src/app/header/header.component.ts
@@ -7,13 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-
-  missionName: string = "Mars 2030";
-  rocketName: string = "Plasma Max";
+  missionName: string = "Apollo 11";
+  rocketName: string = "Saturn V";
+  editingRocket: boolean = false;
+  editingMission: boolean = false;
   
   constructor() { }
 
   ngOnInit() {
   }
 
+  updateMission(updatedName: string) {
+    this.missionName = updatedName;
+    this.editingMission = false;
+  }
+
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
+  }
 }
